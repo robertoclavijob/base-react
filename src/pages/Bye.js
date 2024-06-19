126c841c8aae2f91a2541bfed7e66988b8440c32
Roberto Clavijo <claviro85@gmail.com>
2024-06-18 20:09:21 -0400

    Add bootstrap

--- src/pages/Bye.js
+++ src/pages/Bye.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Bye() {
   return (
-    <h1>See you later!</h1>
+  <>
+   <h1>See you later!</h1>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://lkoirlupclkutltahkie.supabase.co/storage/v1/object/public/images/istockphoto-1443287341-612x612.jpg" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+</>
   );
 }
 
--- src/pages/Hello.js
+++ src/pages/Hello.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function Hello() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <h1>Hello, World!</h1>
+   <>
+	    <h1>Hello, World!</h1>
+	    <Button variant="primary">Primary</Button>{' '}
+            <Button variant="secondary">Secondary</Button>{' '}
+     <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+</>
   );
 }
 
